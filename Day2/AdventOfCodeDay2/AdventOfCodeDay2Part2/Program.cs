89890d52794a0b1651ffa03fc39314845ff16667
Chris Spatig <christopher-spatig@pluralsight.com>
2020-12-02 17:42:52 -0700

    day 2 part 2

---
+++ Day2/AdventOfCodeDay2/AdventOfCodeDay2Part2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCodeDay2Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = "";
+            try
+            {
+                using var sr = new StreamReader("input.txt");
+                inputString = sr.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            var stringList = inputString.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+            var count = 0;
+            
+            foreach (var item in stringList)
+            {
+                var password = item.Split(':').Last();
+                var charReq = item.Split(':').First().Split(' ').Last().ToCharArray().First();
+                var indexOne = int.Parse(item.Split(':').First().Split(' ').First().Split('-').Last());
+                var indexTwo = int.Parse(item.Split(':').First().Split(' ').First().Split('-').First());
+                var indexOneFound = password[indexOne] == charReq;
+                var indexTwoFound = password[indexTwo] == charReq;
+                if (indexOneFound ^ indexTwoFound)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"The count is: {count}");
+        }
+    }
+}

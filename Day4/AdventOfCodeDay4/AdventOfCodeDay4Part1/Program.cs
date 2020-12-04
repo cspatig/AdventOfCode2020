611fc08e605cbd50e265c6e6ae796ad8d2667828
Chris Spatig <christopher-spatig@pluralsight.com>
2020-12-04 13:16:37 -0700

    Day 4 part 1

---
+++ Day4/AdventOfCodeDay4/AdventOfCodeDay4Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCodeDay4Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = "";
+            try
+            {
+                inputString = File.ReadAllText("input.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            var passportList = inputString.Split("\n\n");
+
+            var count = 0;
+
+            foreach (var passport in passportList)
+            {
+                if (passport.Contains("byr") && passport.Contains("iyr") && passport.Contains("eyr") &&
+                    passport.Contains("hgt")
+                    && passport.Contains("hcl") && passport.Contains("ecl") && passport.Contains("pid"))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"The count is: {count}");
+        }
+    }
+}

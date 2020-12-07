cf2515defc95f2d2307c19f5eb85c983310f1b45
Chris Spatig <christopher-spatig@pluralsight.com>
2020-12-06 18:44:32 -0700

    Day 6 Part 1

---
+++ Day6/AdventOfCodeDay6/AdventOfCodeDay6Part1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace AdventOfCodeDay6Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = "";
+            try
+            {
+                inputString = File.ReadAllText("input.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            var groupAnsList = inputString.Split("\n\n").ToList();
+            var sum = 0;
+            
+            foreach (var groupAns in groupAnsList)
+            {
+                var uniqueList = new List<char>();
+                foreach (var character in groupAns)
+                {
+                    if (character != '\r' && character != '\n')
+                    {
+                        uniqueList.Add(character);
+                    }
+                }
+
+                sum += uniqueList.Distinct().Count();
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
